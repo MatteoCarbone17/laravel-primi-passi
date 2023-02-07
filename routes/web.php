2bac3d563557c57561825e7aa1d3e1caf580cbbf
MatteoCarbone17 <matteocarbone17@live.com>
2023-02-07 14:02:32 +0100

    add routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home');
 });
+
+Route::get('/SecondPage', function () {
+    return view('SecondPage');
+});
+
